284255a105a99b566f6e359ab78d44240ecbb0a3
Deepakraj <deepakrajsanthosh@gmail.com>
2025-11-20 23:08:41 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
